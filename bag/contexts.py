8a6d03ea95e93c2dee5cf6484186bf646064b2fc
Roberto Pires <roberto.pires@gmail.com>
2026-04-09 16:29:05 +0100

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,11 +20,10 @@ def bag_contents(request):
             product_count += item_data
 
             bag_items.append({
-    'item_id': item_id,
-    'quantity': quantity,
-    'product': product,
-    'size': size,
-})
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
         else:
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
