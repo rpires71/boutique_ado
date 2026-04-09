ddb1c8e32e9524d2f279ca00ce1aefc27806a05c
Roberto Pires <roberto.pires@gmail.com>
2026-04-09 11:44:55 +0100

    Handle product sizes in bag view and context processor

--- bag/views.py
+++ bag/views.py
@@ -10,14 +10,26 @@ def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag."""
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
 
     bag = request.session.get('bag', {})
 
-    if item_id in bag:
-        bag[item_id] += quantity
+    if size:
+        if item_id in bag:
+            if size in bag[item_id]['items_by_size']:
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in bag:
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
-
     return redirect(redirect_url)
