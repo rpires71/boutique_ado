c60c224398407701820ba239fa7a6c38c6ac14ec
Roberto Pires <roberto.pires@gmail.com>
2026-05-11 21:01:28 +0100

    Add product form

---
+++ products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated labels,
+        and set friendly category names.
+        """
+        super().__init__(*args, **kwargs)
+
+        categories = Category.objects.all()
+        friendly_names = [
+            (category.id, category.get_friendly_name()) for category in categories
+        ]
+
+        self.fields['category'].choices = friendly_names
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
